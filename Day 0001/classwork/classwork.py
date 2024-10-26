0f54c38e4cfc74f431e56b171e23ccfbdeb11119
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-10-26 21:03:52 +0300

    commit

---
+++ Day 0001/classwork/classwork.py
@@ -0,0 +1,17 @@
+name = "irakli"  # ეს არის str (string) ტიპის ცვლადი
+surname = "kvinchia"
+age = 17         # ეს არის int (integer) მთელი რიცხვი
+height = 190.5   # ეს არის float (ათწილადი) ტიპის ცვლადი
+knows_programing = True 
+                         # ეს არის boolean (bool) ტიპის ცვლადი
+is_ugly = False 
+
+print(name + " " + surname)
+
+print(type(age))
+print(type(name))
+print(type(surname))
+print(type(height))
+print(type(knows_programing))
+
+print(name + " " + str(age))
---
+++ Day 0001/homework/lesson1.py
@@ -0,0 +1,6 @@
+name = "irakli"
+surname = "kvinchia"
+age = 17
+height = 190.5
+
+print("me var", name, surname, "var", age, "wlis da simagleshi", height )
