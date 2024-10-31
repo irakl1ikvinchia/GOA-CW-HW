bcbb42302bbb9a5a577b71863427f01433d5c8cd
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-10-31 20:27:36 +0300

    commit

---
+++ Day 0007/classwork/classwork.py
@@ -0,0 +1,21 @@
+num = "20"
+
+print(int(num) + 5)
+print("=================================================")
+
+num1 = 10
+num2 = 10
+
+print(num1 >= num2)
+print(num1 > 5 or num1 < 3)
+print("=================================================")
+
+print(True and True)
+print(True and False)
+print(False and True)
+print(False and False)
+print("=================================================")
+print(True or True)
+print(True or False)
+print(False or True)
+print(False or False)
