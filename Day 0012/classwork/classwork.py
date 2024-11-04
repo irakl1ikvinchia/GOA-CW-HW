db2b28b1d7b91ae12e40e81e94dcf3f2a25eabcd
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-11-04 11:24:34 +0300

    commit

---
+++ Day 0012/classwork/classwork.py
@@ -0,0 +1,25 @@
+# N1
+
+num1 = int(input("please enter num1: "))
+num2 = int(input("please enter num2: "))
+
+for i in range(num1, num2 + 1):
+    print(i)
+
+
+# # N2
+
+num1 = int(input("please enter num1: "))
+num2 = int(input("please enter num2: "))
+sum = 0
+
+for i in range(num1, num2 + 1):
+    sum = sum + i
+
+print(sum)
+
+
+# N3
+
+for i in range(1, 10 + 1):
+    print(i * i)
---
+++ Day 0012/homework/lesson12.py
@@ -0,0 +1,24 @@
+# N1
+
+
+
+# N2
+
+
+
+# N3
+
+
+
+# N4
+
+for i in range(1, 100 +1):
+    if i % 5 == 0:
+        print(i)
+
+
+# N5
+
+for i in range(1, 20 + 1):
+    if i % 6 == 0:
+        print(i)
