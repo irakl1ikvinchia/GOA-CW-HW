70662db39e2e616bb46af36407201b9dacd0d7b2
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-11-04 22:02:38 +0300

    commit

--- Day 0012/homework/lesson12.py
+++ Day 0012/homework/lesson12.py
@@ -1,10 +1,22 @@
 # N1
 
+i = 0
+result = 0
+
+while 1 <= 10:
+    print(i)
+    result = result + i
+    i = i + 1
 
 
 # N2
 
+i = 0
 
+while i <= 20:
+    if i % 3 == 0 and i % 5 == 0:
+        print(i)
+    i = i +1
 
 # N3
 
