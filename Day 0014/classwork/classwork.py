69a9b54cad1a26c5fb18f35ef2c646e410f506a2
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-11-05 22:23:29 +0300

    commit

---
+++ Day 0014/classwork/classwork.py
@@ -0,0 +1,16 @@
+# N1
+
+fav_movies = ["taxi", "tokio drift", "hobs & show", "jumanji"]
+best_movies = fav_movies[1]
+
+print(fav_movies)
+print(fav_movies[2])
+print(best_movies)
+print(fav_movies[-2])
+
+
+# N2
+
+fav_car = input("Enter your car name: ")
+
+print(fav_car)
