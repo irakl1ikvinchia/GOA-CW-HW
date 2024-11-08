427596e5a8136281251dc307f775f1e5a8f07163
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-11-08 20:13:20 +0300

    commit

---
+++ Day 0017/homework/lesson18.py
@@ -0,0 +1,96 @@
+# N1
+
+# def sum_of_numbers(numbers):
+#     result = 0
+#     for i in numbers:
+#         result = result + i
+#     return result
+
+# numbers = [1, 2, 3, 4, 5, 6]
+# print(sum_of_numbers(numbers))
+
+
+# N2
+
+# def filter(str_list):
+#     filtered_list = []
+#     for i in str_list:
+#         if len(i) > 5:
+#             filtered_list.append(i)
+#     return filtered_list
+
+# names = ["irakli", "nodiko", "saba", "luka"]
+
+# print(filter(names))
+
+
+# N3
+
+# def even_numbers(numbers):
+#     filt_list = []
+#     for i in numbers:
+#         if i % 2 == 0:
+#             filt_list.append(i)
+#     return filt_list
+
+# print(even_numbers([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+
+
+# N4
+
+# def max_num(number):
+#     return max(number)
+
+# print(max_num([1,5,2,6,3,89,667,44]))
+
+
+# N5
+
+# def filtered_str(str_lst):
+#     filter_list = []
+
+#     for i in str_lst:
+#         if i[0] == "a":
+#             filter_list.append(i)
+#     return filter_list
+
+# words = ["alpha", "gulia", "mers", "append"]
+# print(filtered_str(words))
+
+
+# N6
+
+# def squared_numbers(numbers):
+#     squared_num_list = []
+
+#     for i in numbers:
+#         squared_num_list.append(i * i)
+
+#     return squared_num_list
+
+# print(squared_numbers([1, 2, 3, 4, 5]))
+
+
+# N7
+
+# def strings_len(str_list):
+#     len_list = []
+
+#     for i in str_list:
+#         len_list.append(len(i))
+#     return len_list
+
+# print(strings_len(["tako", "irakli", "saba", "sandro"]))
+
+
+# N8
+
+# def sum_of_num(numbers):
+#     result = 0
+
+#     for i in numbers:
+#         if i > 10:
+#             result = result + i
+#     return result
+
+# print(sum_of_num([1, 5, 87, 23, 3, 4, 12, 9]))
