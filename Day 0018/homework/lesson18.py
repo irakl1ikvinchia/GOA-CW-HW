96f46c702ceff4a0da1731d01e656972b4d2275d
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-11-09 13:11:51 +0300

    commit

--- Day 0018/homework/lesson18.py
+++ Day 0018/homework/lesson18.py
@@ -25,3 +25,44 @@
 #     return user_list
 
 # print()
+
+
+# N3
+
+# def filter_list(start_num, end_num):
+#     numbers = []
+#     filtered_list = []
+
+#     for i in range(start_num, end_num):
+#         numbers.append(i)
+
+#     for i in numbers:
+#         if i % 2 == 0:
+#             filtered_list.append(i ** 2)
+#         else:
+#             filtered_list.append(i ** 0.5)
+#     return filtered_list
+
+# start_num = int(input("enter start number: "))
+# end_num = int(input("enter end number: "))
+
+# result_list = filter_list(start_num, end_num)
+# print(result_list)
+
+
+# N4
+
+# def sort_lastname(lastname):
+#     char_list = []
+#     even_index = []
+
+#     for i in lastname:
+#         char_list.append(i)
+
+#     for index in range(len(char_list)):
+#         if index % 2 == 0:
+#             even_index.append(char_list[index])
+#     return "".join(even_index)
+
+# lastname = input("Please enter your lastname: ")
+# print(sort_lastname(lastname))
