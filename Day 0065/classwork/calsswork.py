3200c64f254934f5cc95025f3062f986ba21e0f8
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-12-20 17:12:18 +0300

    commit

--- Day 0065/classwork/calsswork.py
+++ Day 0065/classwork/calsswork.py
@@ -1,1 +1,28 @@
 # N1
+# Write a function that takes a string of braces, 
+# and determines if the order of the braces is valid. 
+# It should return true if the string is valid, 
+# and false if it's invalid.
+
+def valid_braces(s):
+    while "()" in s or "[]" in s or "{}" in s:
+        s = s.replace("()", "")
+        s = s.replace("[]", "")
+        s = s.replace("{}", "")
+    return s == ""
+
+
+# N2
+# In this kata you are required to, given a string, 
+# replace every letter with its position in the alphabet.
+
+def alphabet_position(text):
+    result = []
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    
+    for i in text.lower():
+        if i.isalpha():
+            index = alphabet.index(i) + 1
+            result.append(str(index))
+            
+    return " ".join(result)
--- Day 0065/homework/lesson65.py
+++ Day 0065/homework/lesson65.py
@@ -1,2 +1,27 @@
 # N1
-# 
+# Given a string, remove any characters that are unique from the string.
+
+def only_duplicates(st):
+    result = ""
+    for i in st:
+        if st.count(i) != 1:
+            result += i
+    return result
+
+
+# N2
+# Complete the solution so that it splits the 
+# string into pairs of two characters. If the 
+# string contains an odd number of characters 
+# then it should replace the missing second 
+# character of the final pair with an underscore ('_').
+
+def solution(s):
+    if len(s) % 2 != 0:
+        s += "_"
+    result = []
+    
+    for i in range(0, len(s), 2):
+        result.append(s[i] + s[i + 1])
+        
+    return result
