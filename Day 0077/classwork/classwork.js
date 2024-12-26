8509469ad1ed25a287296c4e6c3f529665fd9e26
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-12-26 22:39:13 +0300

    commit

---
+++ Day 0077/classwork/classwork.js
@@ -0,0 +1,15 @@
+const user = {
+    name: "irakli",
+    city: "chxorowku",
+    age: 17,
+    academy: "GOA",
+}
+
+console.log(user)
+
+
+const one = document.getElementById("my-p")
+const second = document.getElementById("your-p")
+
+one.textContent = "irakli"
+second.textContent = "giorgi"
