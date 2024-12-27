019326ffa82b73e01f114e9958b0652b45975f69
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2024-12-27 17:50:49 +0300

    commit

--- Day 0077/classwork/classwork.js
+++ Day 0077/classwork/classwork.js
@@ -13,3 +13,23 @@ const second = document.getElementById("your-p")
 
 one.textContent = "irakli"
 second.textContent = "giorgi"
+
+function greet(){
+    let sayHi = document.getElementById("your-p");
+    sayHi.textContent = "hello"
+}
+
+function colorchange1(){
+    let fun1 = document.getElementById("my-p");
+    fun1.style.color = "red"
+}
+
+function colorchange2(){
+    let fun2 = document.getElementById("my-p");
+    fun2.style.color = "yellow";
+}
+
+function colorchange3(){
+    let fun3 = document.getElementById("my-p");
+    fun3.style.color = "blue";
+}
---
+++ Day 0077/homework/index.js
@@ -0,0 +1,22 @@
+function numPlus(){
+    let plus = document.getElementById("num")
+    plus.textContent = +1
+}
+
+function blue(){
+    let color1 = document.getElementById("block")
+    color1.style.color = "blue"
+    color1.style.backgroundColor = "blue"
+}
+
+function yellow(){
+    let color2 = document.getElementById("block")
+    color2.style.color = "yellow"
+    color2.style.backgroundColor = "yellow"
+}
+
+function green(){
+    let color3 = document.getElementById("block")
+    color3.style.color = "green"
+    color3.style.backgroundColor = "green"
+}
