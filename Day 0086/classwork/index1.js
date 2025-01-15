9dba94e3eca6cf9c0c883e08f3f50573892541c6
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-01-15 12:38:03 +0300

    commit

---
+++ Day 0086/classwork/index1.js
@@ -0,0 +1,9 @@
+const bober = document.getElementsByTagName("p");
+const bob = document.getElementById("my-div")
+
+console.log(bob.parentElement);
+console.log(bob.children);
+console.log(bob.firstElementChild);
+console.log(bob.lastElementChild);
+console.log(bob.previousElementSibling);
+console.log(bob.nextElementSibling);
