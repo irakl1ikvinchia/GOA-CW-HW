4eddb8a2facb309ff9d5868a16027dccb67b0ba5
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-01-19 21:24:33 +0300

    commit

--- Day 0089/classwork/index.js
+++ Day 0089/classwork/index.js
@@ -1,18 +1,34 @@
 const box = document.getElementById("div2");
 
-let position = 0;
-let direction = 1;
+let positionY = 0;
+let positionX = 0;
+let direction = "right";
 
 function animateBox() {
-    position += direction;
-
-    if (position >= 400){
-        direction = -1;
-    } else if (position <= 0){
-        direction = 1;
+    if (direction === "right") {
+        positionX++;
+        if(positionX >= 400){
+            direction = "bottom";
+        }
+    } else if (direction === "bottom") {
+        positionY++;
+        if (positionY >= 400) {
+            direction = "left";
+        }
+    } else if (direction === "left") {
+        positionX--;
+        if (positionX <= 0) {
+            direction = "top";
+        }
+    } else if (direction === "top") {
+        positionY--;
+        if (positionY <= 0){
+            direction = "right";
+        }
     }
 
-    box.style.left = position + "px";
+    box.style.left = positionX + "px";
+    box.style.top = positionY + "px";
 }
 
 setInterval(animateBox, 20);
