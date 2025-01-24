22783f6b11c5bc8b8b12c58a7daf56938ea869f6
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-01-24 17:13:22 +0300

    commit

--- Day 0092/classwork/classwork.js
+++ Day 0092/classwork/classwork.js
@@ -1,5 +1,16 @@
+// N1
+
 const cube = number => {
     return number ** 3;
 }
 
 console.log(cube(5))
+
+
+// N2
+
+const sum = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(sum (44,4))
