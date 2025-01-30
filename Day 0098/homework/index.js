051d50bd6b01d53d8e5a9c798d06c59e386639ad
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-01-30 20:42:12 +0300

    commit

--- Day 0098/homework/index.js
+++ Day 0098/homework/index.js
@@ -1,6 +1,14 @@
-class Account {
+class Human {
     constructor(name, lastname) {
         this.name = name;
         this.lastname = lastname;
     }
+
+    printInfo() {
+        console.log(this.name, lastname);
+    }
 }
+
+const human1 = new Human("irakli", "kvinchia")
+
+console.log(human1)
