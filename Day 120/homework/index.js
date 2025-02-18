964375135ec72e666b1bc43fc9ba68b47f5f0a1d
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-02-18 21:42:00 +0300

    commit

---
+++ Day 120/homework/index.js
@@ -0,0 +1,10 @@
+const greet = (name) => {
+    console.log(`hello, ${name}`)
+  }
+  
+  const study = (academy) => {
+    console.log(`i'm study in, ${academy}`)
+  }
+  
+  module.exports = greet;
+  module.exports = study;
---
+++ Day 120/homework/main.js
@@ -0,0 +1,7 @@
+const greet = require('./index1.js');
+const study = require('./index1.js');
+
+console.log(greet);
+greet("irakli")
+console.log(study);
+study("Goal-oriented-academy GOA")
