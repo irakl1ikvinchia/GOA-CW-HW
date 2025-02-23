f156dba503e08a9316e329347045086bb4eaa166
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-02-23 17:01:05 +0300

    commit

---
+++ Day 0124/classwork/index.js
@@ -0,0 +1,11 @@
+const p = (
+    <p id="1">
+        <b>
+            <i>Hello world</i>
+        </b>
+    </p>
+);
+
+const root = ReactDom.createElement(document.getElementById("root"));
+
+root.render(p);
