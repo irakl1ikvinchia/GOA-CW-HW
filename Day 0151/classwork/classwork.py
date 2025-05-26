9c9b3e1795edcc02c10deef8a6a4cee1c8eed879
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-05-26 20:34:28 +0300

    commit

---
+++ Day 0151/classwork/classwork.py
@@ -0,0 +1,7 @@
+# /Return a string's even characters.
+
+def even_chars(st): 
+    if len(st) < 2 or len(st) > 100:
+        return "invalid string"
+
+    return [st[i] for i in range(1, len(st), 2)]
