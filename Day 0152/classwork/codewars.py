46886ac8b540cd4ce64b14ce0ad48060f3c008da
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-06-14 20:47:55 +0300

    commit

--- Day 0152/classwork/codewars.py
+++ Day 0152/classwork/codewars.py
@@ -5,3 +5,15 @@
 
 # def age(x, y):
 #     return (x * y) / (y - 1)
+
+
+
+# def strong_enough(earthquake, age):
+#     from math import prod
+
+#     shockwaves = [sum(tremor) for tremor in earthquake]
+#     magnitude = prod(shockwaves)
+
+#     strength = 1000 * (0.99 ** age)
+
+#     return "Safe!" if strength >= magnitude else "Needs Reinforcement!"
