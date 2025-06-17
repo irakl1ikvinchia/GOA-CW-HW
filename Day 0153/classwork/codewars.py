408d18ddd91e6dc2e17d192db5f082b4373eb9cd
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-06-17 21:00:36 +0300

    commit

---
+++ Day 0153/classwork/codewars.py
@@ -0,0 +1,7 @@
+# def string_to_int_list(s):
+#     return [int(num) for num in s.split(',') if num.strip() != '']
+
+# def remove_exclamation_marks(s):
+#     new_s = s.replace("!", "")
+    
+#     return new_s
