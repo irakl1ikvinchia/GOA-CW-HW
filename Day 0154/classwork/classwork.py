3dfad483f3cdd71bab7d4ebfe73a1c9fd62c719f
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-06-20 20:56:50 +0300

    commit

--- Day 0154/classwork/classwork.py
+++ Day 0154/classwork/classwork.py
@@ -1,3 +1,14 @@
-def word_search(query, seq):
-    result = [s for s in seq if query.lower() in s.lower()]
-    return result if result else ["None"]
+# def word_search(query, seq):
+#     result = [s for s in seq if query.lower() in s.lower()]
+#     return result if result else ["None"]
+
+
+def calc_type(a, b, res):
+    if a + b == res:
+        return 'addition'
+    if a - b == res:
+        return 'subtraction'
+    if a * b == res:
+        return 'multiplication'
+    if a / b == res:
+        return 'division'
