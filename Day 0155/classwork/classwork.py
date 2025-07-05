52c4aa3bd6b71ce8da20096417c70a99e620f6e6
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-07-05 20:40:26 +0300

    commit

---
+++ Day 0155/classwork/classwork.py
@@ -0,0 +1,10 @@
+# import random
+
+# def r_p_s_cheat(opponent_move):
+#     wins_against = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+#     loses_against = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+    
+#     if random.random() < 0.9:
+#         return wins_against[opponent_move]
+#     else:
+#         return loses_against[opponent_move]
