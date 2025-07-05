6bb9eda90e21489348655f1cfc81ec59511975ea
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-07-05 21:40:03 +0300

    commit

--- Day 0155/classwork/classwork.py
+++ Day 0155/classwork/classwork.py
@@ -22,3 +22,16 @@
 #         total += n
 #         n //= 2
 #     return total
+
+
+# def valid_card(card):
+#     card = card[::-1].replace(" ", "")
+#     new_str = []
+#     for i in range(len(card)):
+#         if i % 2 != 0 and int(card[i]) * 2 > 9:
+#             new_str.append(int(card[i]) * 2 - 9)
+#         elif i % 2 != 0 and int(card[i]) * 2 < 10:
+#             new_str.append(int(card[i]) * 2)
+#         else:
+#             new_str.append(int(card[i]))
+#     return sum(new_str) % 10 == 0
