4c9bf42025acaa032a2d6ca3f504a0f49cc88ec0
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-07-07 20:26:46 +0300

    ommit

--- Day 0155/homework/lesson.py
+++ Day 0155/homework/lesson.py
@@ -1,2 +1,7 @@
 # def solve(a,b):
 #     return ''.join([ch for ch in a if ch not in b] + [ch for ch in b if ch not in a])
+
+
+# def largest_sum(s):
+#     substrings = s.split('0')
+#     return max(sum(int(digit) for digit in part) for part in substrings)
