feeca327d2259affecd917bd3c58a2e2aa59144a
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-07-08 11:42:22 +0300

    commit

---
+++ Day 0156/classwork/classwork.py
@@ -0,0 +1,11 @@
+# def is_opposite(s1,s2):
+#     return bool(s1) and s1.swapcase() == s2
+
+
+
+# def scrabble_score(st): 
+#     total = 0
+#     for i in st:
+#         if i.isalpha():
+#             total += dict_scores[i.upper()]
+#     return total
