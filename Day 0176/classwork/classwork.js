1bd1de81eb5f892099edd4adf6d4b2eece6d2be5
iraklikvinchia8Agmail.com <iraklikvinchia8@gmail.com>
2025-10-01 14:14:11 +0300

    commit

--- Day 0176/classwork/classwork.js
+++ Day 0176/classwork/classwork.js
@@ -1,1 +1,15 @@
-// 
+function getMostFrequent(json) {
+    return json.temperature.map(day => {
+    const freq = {};
+    let max = 0;
+
+    day.forEach(temp => {
+      freq[temp] = (freq[temp] || 0) + 1;
+      if (freq[temp] > max) max = freq[temp];
+    });
+
+    for (let i = day.length - 1; i >= 0; i--) {
+      if (freq[day[i]] === max) return day[i];
+    }
+  });
+}
